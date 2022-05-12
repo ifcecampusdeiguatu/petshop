bbdf7d005b34a348106ddd70f57d80f901433ee8
nalissonouther <nalisson35@gmail.com>
2022-05-12 13:37:37 -0300

    Create Tutor.java

---
+++ src/main/java/com/br/petshop/src/petshop/model/Tutor.java
@@ -0,0 +1,52 @@
+package com.br.petshop.src.petshop.model;
+
+public Tutor {
+    private int id;
+    private String nome;
+    private String raca;
+    private String especie;
+
+    // Construtor especial
+    public Animal(String nome, String raca, String especie) {
+        this.nome = nome;
+        this.raca = raca;
+        this.especie = especie;
+    }
+
+    // Construtor padrão
+    public Animal() {
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return this.raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getEspecie() {
+        return this.especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+}
